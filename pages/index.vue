7c0620be8f2ee1b039b6b8033a7c8129c6ff16a9
xTeKc <bcd3v@pm.me>
2021-09-14 22:59:21 -0400

    extern and intern links

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,8 @@
     <NuxtLink to="/about">
       About (internal link that belongs to the Nuxt App)
     </NuxtLink>
+    <br>
+    <br>
     <a href="https://nuxtjs.org">External Link to another page</a>
   </main>
 </template>
