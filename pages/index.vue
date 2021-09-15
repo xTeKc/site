c7dc5c141090413599f00f1a6cdff9d1b652d843
xTeKc <bcd3v@pm.me>
2021-09-14 22:56:13 -0400

    add link

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <NuxtLink to="/">Home page</NuxtLink>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
 </template>
