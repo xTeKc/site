fdc1370817e4e945703b27607aba6e3a763ac878
xTeKc <bcd3v@pm.me>
2021-09-14 23:27:50 -0400

    add template

--- components/components.js
+++ components/components.js
@@ -2,3 +2,5 @@ Vue.component('blog-list', { props: ['limit'], data: function(){  return {   art
 
 created: function(){  var query = deliveryClient   .items()   .type('blog_post')   .elementsParameter(['link', 'title', 'image_url', 'image', 'teaser'])   .orderParameter('elements.published', SortOrder.desc);   if (this.limit){   query = query.limitParameter(this.limit);  }  query   .getPromise()   .then(response =>    this.$data.articles = response.items.map(item => ({     url: item.link.value,     header: item.title.value,     image: item.image_url.value != '' ? item.image_url.value : item.image.assets[0].url,     teaser: item.teaser.value    }))   ); },
 
+template: `  <section class="features">   <article v-for="article in articles">    ...   </article>  </section> ` });
+
