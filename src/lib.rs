bb4d94dcb2dad18340399fdb84d9718096d7672a
xTeKc <bcd3v@pm.me>
2021-09-17 22:27:07 -0400

    modify contents: add more wasm funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,102 @@
-mod utils;
+// mod utils;
+
+// use wasm_bindgen::prelude::*;
+
+// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// // allocator.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+// }
+
+// #[wasm_bindgen]
+// pub fn greet() {
+//     alert("Hello, wasm-site!");
+// }
+
+
+
+
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
+// JS doesn't have a chars type which means:
+// - The _c argument is the first char of a JS string.
+// - The char returned will be a JS string.
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub fn char_example(_c: char) -> char {
+    '🚀'
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-site!");
+pub fn string_example(s: String) -> String {
+    format!("Hello {}", s)
+}
+
+// str cannot be used as a return type.
+// This is because we can't return borrowed references with the wasm_bindgen macro.
+#[wasm_bindgen]
+pub fn str_example(s: &str) -> String {
+    format!("Hello {}", s)
+}
+
+#[wasm_bindgen]
+pub fn number_example(n: i32) -> i32 { // assume the same for u32, usize, etc.
+    n+100
+}
+
+#[wasm_bindgen]
+pub fn bool_example(_b: bool) -> bool {
+    true
+}
+
+// `Box<[JsValue]>` are the representation for a JS array object.
+// When it comes to Js Arrays:
+// - They are iterable.
+// - Can contain multiple types by being of type JsValue (strictly typed arrays exist for numbers).
+// - Don't really support N-dimensional arrays and are expensive to work with.
+#[wasm_bindgen]
+pub fn mixed_array_example(array: Box<[JsValue]>) -> Box<[JsValue]> {
+    for value in array.iter() {
+        // compute things...
+    }
+
+    vec![
+        "Hello".into(),
+        512.into(),
+        JsValue::NULL,
+        JsValue::UNDEFINED,
+        61.20.into(),
+    ]
+    .into_boxed_slice()
+}
+
+// Typed arrays are only available for number types.
+// For example, the function below will return a JS Int32Array type.
+#[wasm_bindgen]
+pub fn typed_array_example(_array: Box<[i32]>) -> Box<[i32]> {
+    vec![1, 2, 3, 4, 5, 6, 7].into_boxed_slice()
+}
+
+// When it comes to Option:
+// - Some returns the value inside.
+// - None returns a JS undefined.
+#[wasm_bindgen(catch)]
+pub fn option_example() -> Option<i32> {
+    None
+}
+
+// When it comes to Result
+// - Result<T, JsValue> is the only supported signature. T must be convertible to a JsValue.
+// - #[wasm_bindgen(catch)] must be used when returning a result.
+// - Err will be equivalent to a JS thrown error.
+// - Ok will return the value inside.
+#[wasm_bindgen]
+pub fn result_example() -> Result<i32, JsValue> {
+    // With the wasm prelude imported, we can convert most common types by calling .into()
+    Err("JS error!".into())
 }
