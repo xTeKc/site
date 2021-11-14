3b70897d263352f0ed06515f80c42c395a3f79c3
xTeKc <bcd3v@pm.me>
2021-11-14 18:14:43 -0500

    add data for create func

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -21,7 +21,21 @@ impl Component for Home {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self {}
+        let items: Vec<Item> = vec![
+            Item {
+                id: 1,
+                name: "Apple".to_string(),
+                description: "An apple a day keeps the doctor away".to_string(),
+                image: "image".to_string(),
+                price: 4.49,
+            },
+        ];
+
+        Self {
+            state: State {
+                items,
+             }
+        }
     }
 
     fn update(&mut self, _: Self::Message) -> ShouldRender {
