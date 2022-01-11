f78dcc0488e2f4acc40bddb279dcf8be6a282f50
xTeKc <bcd3v@pm.me>
2022-01-10 20:42:54 -0500

    update about page: dir back to home page

--- src/templates/about.rs
+++ src/templates/about.rs
@@ -1,21 +1,57 @@
-use perseus::Template;
-use sycamore::prelude::{component, view, Html, SsrNode, View};
+// update "greeting" to something else
+
+use perseus::{
+    http::header::{HeaderMap, HeaderName},
+    Html, RenderFnResultWithCause, SsrNode, Template,
+};
+use serde::{Deserialize, Serialize};
+use sycamore::prelude::{component, view, View};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AboutPageProps {
+    pub greeting: String,
+}
 
 #[perseus::template(AboutPage)]
 #[component(AboutPage<G>)]
-pub fn about_page() -> View<G> {
+pub fn about_page(props: AboutPageProps) -> View<G> {
     view! {
-        p { "About." }
-    }
-}
-
-#[perseus::head]
-pub fn head() -> View<SsrNode> {
-    view! {
-        title { "About Page | Basic" }
+        p {(props.greeting)}
+        a(href = "index", id = "index-link") { "Home!" }
     }
 }
 
 pub fn get_template<G: Html>() -> Template<G> {
-    Template::new("about").template(about_page).head(head)
+    Template::new("about")
+        .build_state_fn(get_build_props)
+        .template(about_page)
+        .head(head)
+        .set_headers_fn(set_headers)
+}
+
+#[perseus::head]
+pub fn head(_props: AboutPageProps) -> View<SsrNode> {
+    view! {
+        title { "About Page | Web App" }
+    }
+}
+
+#[perseus::autoserde(build_state)]
+pub async fn get_build_props(
+    _path: String,
+    _locale: String,
+) -> RenderFnResultWithCause<AboutPageProps> {
+    Ok(AboutPageProps {
+        greeting: "Hello from About page!".to_string(),
+    })
+}
+
+#[perseus::autoserde(set_headers)]
+pub fn set_headers(props: Option<AboutPageProps>) -> HeaderMap {
+    let mut map = HeaderMap::new();
+    map.insert(
+        HeaderName::from_lowercase(b"x-greeting").unwrap(),
+        props.unwrap().greeting.parse().unwrap(),
+    );
+    map
 }
