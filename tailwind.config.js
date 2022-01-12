d37087148313997cb6051cd44232fe0974b5b6a4
xTeKc <bcd3v@pm.me>
2022-01-12 16:03:46 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-    content: [],
+    content: [
+        "./src/**/*.{rs,html,css}",
+        "./index.html",
+        "./static/styles/**/*.css",
+    ],
     theme: {
-      extend: {},
+        fontFamily: {
+            heading: ["Comfortaa", "sans"],
+            ...defaultTheme.fontFamily,
+        },
+        screens: {
+            "2xs": "370px",
+            xs: "475px",
+            ...defaultTheme.screens,
+        },
+        colors: {
+            // Full color palette
+            transparent: "transparent",
+            current: "currentColor",
+            // navy: "#001122",
+            // "navy-deep": "#000B14",
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.amber,
+            green: colors.emerald,
+            indigo: colors.indigo,
+        },
     },
-    plugins: [],
-  }
+    variants: {},
+    plugins: [require("tailwind-hamburgers")],
+};
